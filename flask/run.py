d1bddbecf7bf2dca8f65319a7fe3311ee75b6109
shegsteham <oluwasegunadepoju@gmail.com>
2019-12-27 10:40:05 +0100

    init entry file

--- flask/run.py
+++ flask/run.py
@@ -1,8 +1,11 @@
 import os
 from app.__init__ import createApp
 from app.helpers.catchRouteErrors import catchRouteErrors
+from app.views import addResources
 
 APP = createApp()
+
+addResources(APP)
 
 catchRouteErrors(APP)
 
