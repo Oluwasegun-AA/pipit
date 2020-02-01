3fe5704a09a34b2506565cce12622f42676cb383
shegsteham <oluwasegunadepoju@gmail.com>
2020-02-01 10:40:05 +0100

    ft():update views

--- flask/app/views/authViews.py
+++ flask/app/views/authViews.py
@@ -1,13 +1,18 @@
 from flask import jsonify
 from flask_restful import Resource
 from app.helpers.request import getRequest
+from app.models.authModels import authModel
+
+Auth = authModel()
 
 class auth(Resource):
   def post(self):
-    data = getRequest()
-    return jsonify({'message': 'Login successful',  'data':data})
+    req = getRequest()
+    data = Auth.login(req)
+    return jsonify(data)
 
 class signup(Resource):
   def post(self):
-    data = getRequest()
-    return jsonify({'message': 'signup successful', 'data': data})
+    req = getRequest()
+    data = Auth.signup(req)
+    return jsonify(data)
