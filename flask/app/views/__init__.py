9e5b37282e430db5ab48fdff5735c8eee07b435e
shegsteham <oluwasegunadepoju@gmail.com>
2020-05-01 10:40:05 +0100

    ft():introduce baseUrl

--- flask/app/views/__init__.py
+++ flask/app/views/__init__.py
@@ -1,8 +1,10 @@
 '''app/views/__init__.py'''
 from app.views.authViews import auth, signup
 from flask_restful import Api
+import os
 
 def addResources(app):
-   api = Api(app, catch_all_404s=True)
-   api.add_resource(auth, '/login')
-   api.add_resource(signup, '/signup')
+  BASE = os.getenv('BASEURL')
+  api = Api(app, catch_all_404s=True)
+  api.add_resource(auth, BASE+'/login')
+  api.add_resource(signup, BASE+'/signup')
