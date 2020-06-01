3c74896dc799376ea41e519f77b490220e3f6d7e
shegsteham <oluwasegunadepoju@gmail.com>
2020-06-01 10:40:05 +0100

    ft(): implement userModels

---
+++ flask/app/models/usersModels.py
@@ -0,0 +1,38 @@
+from app.models.baseModel import baseModel
+from app.helpers.fetchData import fetch, fetchMany
+from app.database.getQueryFromDict import getQueryFromDict
+
+class UsersModel(baseModel):
+  def getUser(self, user):
+    data = fetch(self.cursor, 'Users', user['username'])
+    return data
+
+  def getAllUser(self):
+    data = fetchMany(self.cursor, 'Users')
+    return data
+
+  def updateUser(self, id, data):
+    values = list(data.values())
+    values.append(id)
+    self.cursor.execute('''UPDATE "Users" SET username = (%s), email = (%s), "firstName" = (%s), "lastName" = (%s) WHERE id = (%s);''', values)
+    self.connection.commit()
+    data = fetch(self.cursor, 'Users', id, 'where id = (%s);')
+    return data
+
+  def deleteUser(self, id):
+    self.cursor.execute('''DELETE FROM "Users" WHERE id = (%s);''', [id])
+    self.connection.commit()
+    data = fetch(self.cursor, 'Users', id, 'where id = (%s);')
+    return data
+
+  def makeAdmin(self, id, data):
+    self.cursor.execute('''UPDATE "Users" SET "isAdmin" = (%s) WHERE id = (%s);''', [data['isAdmin'], id])
+    self.connection.commit()
+    data = fetch(self.cursor, 'Users', id, 'where id = (%s);')
+    return data
+
+  def verifyUser(self, id):
+    self.cursor.execute('''UPDATE "Users" SET verified = (%s) WHERE id = (%s);''', [True, id])
+    self.connection.commit()
+    data = fetch(self.cursor, 'Users', id, 'where id = (%s);')
+    return data
