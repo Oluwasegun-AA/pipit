255809ab8d5481c540de243459cf72071fa9be7c
shegsteham <oluwasegunadepoju@gmail.com>
2020-07-01 10:40:05 +0100

    ft(): implement userViews

--- flask/app/views/__init__.py
+++ flask/app/views/__init__.py
@@ -1,10 +1,16 @@
 '''app/views/__init__.py'''
 from app.views.authViews import auth, signup
+from app.views.usersViews import (User, GetAllUser, VerifyUser, makeAdmin)
 from flask_restful import Api
 import os
 
 def addResources(app):
-  BASE = os.getenv('BASEURL')
+  BASE = os.getenv('BASE_URL')
   api = Api(app, catch_all_404s=True)
   api.add_resource(auth, BASE+'/login')
   api.add_resource(signup, BASE+'/signup')
+
+  api.add_resource(User, BASE+'/user', BASE+'/user/<id>/')
+  api.add_resource(GetAllUser, BASE+'/users')
+  api.add_resource(VerifyUser, BASE+'/user/verify/<id>')
+  api.add_resource(makeAdmin, BASE+'/user/authorize/<id>')
---
+++ flask/app/views/usersViews.py
@@ -0,0 +1,46 @@
+from flask import jsonify
+from flask_restful import Resource
+from app.helpers.request import getRequest
+from app.models.authModels import authModel
+from app.models.usersModels import UsersModel
+from app.helpers.pick import pick
+
+Auth = authModel()
+Users = UsersModel()
+
+
+class User(Resource):
+  def get(self):
+    user = getRequest()
+    response = Users.getUser(user)
+    return jsonify(response)
+
+  def patch(self, id):
+    data = pick(getRequest(), ['username', 'email', 'firstName', 'lastName'])
+    response = Users.updateUser(str(id), data)
+    return jsonify(response)
+
+  def delete(self, id):
+    response = Users.deleteUser(str(id))
+    return jsonify(response)
+
+  def post(self):
+    req = getRequest()
+    data = Auth.signup(req)
+    return jsonify(data)
+
+class GetAllUser(Resource):
+  def get(self):
+    response = Users.getAllUser()
+    return jsonify(response)
+
+class VerifyUser(Resource):
+  def patch(self, id):
+    response = Users.verifyUser(str(id))
+    return jsonify(response)
+
+class makeAdmin(Resource):
+  def patch(self, id):
+    req = pick(getRequest(), ['isAdmin'])
+    response = Users.makeAdmin(str(id), req)
+    return jsonify(response)
